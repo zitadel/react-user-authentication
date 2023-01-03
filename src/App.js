6fc545941c74a531d8a670bd4dade3d727a2d355
Max Peintner <max@caos.ch>
2023-01-03 17:20:21 +0100

    remove pkce

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from 'react-router-dom';
-import Login from './components/Login';
-import Callback from './components/Callback';
-import authConfig from './authConfig';
-import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Callback from "./components/Callback";
+import authConfig from "./authConfig";
+import { UserManager, WebStorageStateStore } from "oidc-client-ts";
 
 function App() {
-    const userManager = new UserManager({
-        userStore: new WebStorageStateStore({ store: window.localStorage }),
-        ...authConfig
+  const userManager = new UserManager({
+    userStore: new WebStorageStateStore({ store: window.localStorage }),
+    ...authConfig,
+  });
+
+  function authorize() {
+    userManager.signinRedirect({ state: "a2123a67ff11413fa19217a9ea0fbad5" });
+  }
+
+  function clearAuth() {
+    userManager.signoutRedirect();
+  }
+
+  const [authenticated, setAuthenticated] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    userManager.getUser().then((user) => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
     });
+  }, [userManager]);
 
-    function authorize(codeChallenge) {
-        userManager.signinRedirect({ state: 'a2123a67ff11413fa19217a9ea0fbad5', codeChallenge });
-    }
-
-    function clearAuth() {
-        userManager.signoutRedirect();
-    }
-
-    const [authenticated, setAuthenticated] = useState(null);
-    const [userInfo, setUserInfo] = useState(null);
-
-    useEffect(() => {
-        userManager.getUser().then((user) => {
-            if (user) {
-                setAuthenticated(true);
-            } else {
-                setAuthenticated(false);
-            }
-        });
-    }, [userManager]);
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Login auth={authenticated} handleLogin={authorize}/>} />
-                <Route path='/callback' element={<Callback
-                    auth={authenticated}
-                    setAuth={setAuthenticated}
-                    userInfo={userInfo}
-                    setUserInfo={setUserInfo}
-                    handleLogout={clearAuth}
-                    userManager={userManager}
-                />
-                } />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Login auth={authenticated} handleLogin={authorize} />}
+        />
+        <Route
+          path="/callback"
+          element={
+            <Callback
+              auth={authenticated}
+              setAuth={setAuthenticated}
+              userInfo={userInfo}
+              setUserInfo={setUserInfo}
+              handleLogout={clearAuth}
+              userManager={userManager}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
